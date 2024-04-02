0eff75fd0bc8ac68ece8bff854a27c725f5b3b5a
rohit gonnade <rohit.gonnade@autodesk.com>
2024-04-02 09:16:11 +0530

    Use of single lib file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+
 #include "print.h"
 #include "second_library.h"
+#include "advance_print.h"
 #include <iostream>
 
 int main()
 {
     printMe printMe;
     Second_Library sL;
+    Advance_print ap;
     std::cout << "Hello " << printMe.value() << " \n";
     std::cout << "2 Multiply by 10 = " << sL.mutiply_by_10(2) << " \n";
+    std::cout << ap.value();
 }
